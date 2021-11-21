06dacff9d14decf1369dfc2284019880f3af1fb3
Nttwt Miz Chin <mix2zeta@gmail.com>
2021-11-21 18:16:56 +0700

    Create profile.ts

---
+++ profile.ts
@@ -0,0 +1,13 @@
+import { Profile } from 'mix2zeta';
+
+Profile.name = Miz;
+Profile.location = BKK/TH;
+Profile.blog = 'https://medium.com/@mix2zeta';
+Profile.developer = {
+  company: @wisesight,
+  position: ['backend-devloper', 'frontend-developer', 'devOps', 'productOwnerAnnoyer'],
+  language: ['python', 'javascript', 'typescript'],
+  techStack: ['node', 'react', 'mongodb', 'mariadb', 'workflow', 'docker', 'aws'],
+}
+Profile.setPassiveLevel('💤')
+Profile.save();
